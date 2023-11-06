8f8bd509bc76ab53ee8fde7b8c49e98e2044004c
S. Nithin Kumar(66D4) and G. Sai Puneeth(66B2) Geethanjali College Of Engineering 1st year(AIML-C)  2022-26 <85955710+nithin521@users.noreply.github.com>
2023-11-06 22:38:56 +0530

    Add files via upload

---
+++ eventHosting.js
@@ -0,0 +1,24 @@
+const back= document.getElementById('back');
+const button = document.getElementById('button');
+const eventName = document.getElementById('eventName');
+const eventDate = document.getElementById('eventDate');
+const eventDescription = document.getElementById('eventDescription');
+const eventImage = document.getElementById('eventImage');
+console.log(back);
+back.addEventListener('click',()=>{
+    window.location.href="/home/index.html"
+})
+button.addEventListener('click',(event)=>{
+    const eventName = document.getElementById('eventName');
+    const eventDate = document.getElementById('eventDate');
+    const eventDescription = document.getElementById('eventDescription');
+    const eventImage = document.getElementById('eventImage');
+
+    if (eventName.checkValidity() && eventDate.checkValidity() && eventDescription.checkValidity() && eventImage.checkValidity()) {
+        alert('You have successfully uploaded your event 😀😀😀');
+        event.preventDefault();
+        window.location.href="/home/index.html"
+    } else {
+        alert('Please fill in all required fields before submitting.');
+    }
+})
